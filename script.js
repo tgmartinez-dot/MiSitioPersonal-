c56a89f188636a5d8e3e4ad8ace885418e53dd43
tgmartinez-dot <tg.martinez@ebc.edu.mx>
2026-06-28 18:12:25 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensaje() {
+    document.getElementById("mensaje").innerHTML =
+        "¡Gracias por visitar mi sitio web! Espero que conozcas un poco más sobre mí y mis avances en la materia.";
+}
