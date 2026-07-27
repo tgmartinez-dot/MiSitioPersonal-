6f408ed005ca618820c21cb2a49b6f6ec1a05015
tgmartinez-dot <tg.martinez@ebc.edu.mx>
2026-07-26 21:13:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ function mostrarMensaje() {
     document.getElementById("mensaje").innerHTML =
         "¡Gracias por visitar mi sitio web! Espero que conozcas un poco más sobre mí y mis avances en la materia.";
 }
+unction mostrarProyecto() {
+
+    let detalle = document.getElementById("detalleProyecto");
+
+    if (detalle.innerHTML === "") {
+
+        detalle.innerHTML =
+        "Actualmente estoy desarrollando proyectos académicos utilizando HTML, CSS y JavaScript. Este portafolio seguirá creciendo con nuevos proyectos durante el curso.";
+
+    } else {
+
+        detalle.innerHTML = "";
+
+    }
+
+}
